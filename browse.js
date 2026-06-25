b61c5d8a21940d5eebec0bf80ed56c917357d199
rinka-bit <merchenta.store@gmail.com>
2026-06-25 16:05:30 +0700

    Update browse.js

--- browse.js
+++ browse.js
@@ -420,57 +420,159 @@ function renderProducts(products){
 
 function createProductCard(product){
 
-    return `
-    <div
-    class="card"
-    onclick="
-    window.location.href=
-    'product.html?id=${product.product_id}'
-    ">
+return `
 
-        <img
-        src="${product.image}"
-        alt="${product.name}"
-        loading="lazy">
+<div
+class="card"
+onclick="
+window.location.href=
+'product.html?id=${product.product_id}'
+">
 
-        ${
-            product.new_arrival
-            ?
-            `<div class="badge-new">NEW</div>`
-            :
-            ""
-        }
+<div class="image-box">
 
-        ${
-            product.product_type==="preorder"
-            ?
-            `<div class="badge-preorder">PREORDER</div>`
-            :
-            ""
-        }
+<img
+src="${product.image}"
+alt="${product.name}"
+loading="lazy">
 
-        ${
-            getDeadlineText(
-                product.preorder_deadline
-            )
-            ?
-            `<div class="deadline-badge">
-            ${getDeadlineText(product.preorder_deadline)}
-            </div>`
-            :
-            ""
-        }
+${
 
-        <h3>${product.name}</h3>
+product.new_arrival
 
-        <p>${product.fandom||""}</p>
+?
 
-        <p>
-        ฿${product.sale_price || product.price}
-        </p>
+`<div class="badge-new">
+NEW
+</div>`
 
-    </div>
-    `;
+:
+
+""
+
+}
+
+${
+
+product.on_sale
+
+?
+
+`<div class="badge-sale">
+SALE
+</div>`
+
+:
+
+""
+
+}
+
+${
+
+product.product_type==="preorder"
+
+?
+
+`<div class="badge-preorder">
+PREORDER
+</div>`
+
+:
+
+`<div class="badge-stock">
+พร้อมส่ง
+</div>`
+
+}
+
+</div>
+
+<div class="card-body">
+
+<h3>
+
+${product.name}
+
+</h3>
+
+<p class="fandom">
+
+${product.fandom||""}
+
+</p>
+
+<div class="price-box">
+
+${
+
+product.on_sale
+
+?
+
+`
+
+<div class="sale-price">
+
+฿${product.price}
+
+</div>
+
+<div class="old-price">
+
+฿${product.old_price}
+
+</div>
+
+`
+
+:
+
+`
+
+<div class="normal-price">
+
+฿${product.price}
+
+</div>
+
+`
+
+}
+
+</div>
+
+${
+
+getDeadlineText(
+product.preorder_deadline
+)
+
+?
+
+`
+
+<div class="deadline">
+
+${getDeadlineText(
+product.preorder_deadline
+)}
+
+</div>
+
+`
+
+:
+
+""
+
+}
+
+</div>
+
+</div>
+
+`;
 
 }
 
